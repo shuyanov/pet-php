7562c37a362d6b9ec0a771bedc6a1c46ef5725de
shuyanov <shuyanov@shuyanovs-MacBook-Pro.local>
2024-03-30 13:12:19 +0300

    feat(value): create value

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-<h1>Test mamp</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    define(PI, 3.14);//constants
+    $number = 10; // int
+    $negativeNumber = -10; // int
+    $numberTwo = 1.40; //float
+    $negativeNumberTwo = -1.40; //float
+    $str = "1.40"; //string
+    $boolean = false; //boolean
+    
+    echo PI + $number;
+    ?>
+</body>
+
+</html>
